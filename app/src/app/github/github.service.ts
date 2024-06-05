2e53cbf306f62338fe16f500b9da095494c1d114
Alexander Ciesielski <alexander@ciesielski.co>
2024-06-05 17:08:56 +0200

    fix

--- app/src/app/github/github.service.ts
+++ app/src/app/github/github.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { of, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
+import { GITHUB_PUBLIC_EVENTS_RESPONSE } from './public-events-response';
+
+type GithubEventResponse = typeof GITHUB_PUBLIC_EVENTS_RESPONSE;
 
 export type GithubEventType =
   | 'IssuesEvent'
@@ -17,12 +20,12 @@ export class GithubEventService {
   private readonly lastFetched = localStorage.getItem('github-last-fetched');
   private readonly cachedEvents = localStorage.getItem('github-events');
 
-  fetchAll() {
+  fetchAll(): Observable<GithubEventResponse> {
     if (this.lastFetched && this.cachedEvents && Date.now() - Number(this.lastFetched) < 1000 * 60 * 5) {
       return of(JSON.parse(this.cachedEvents));
     }
 
-    return this.http.get('/api/v1/github-activity.json').pipe(
+    return this.http.get<GithubEventResponse>('/api/v1/github-activity.json').pipe(
       tap((events) => {
         localStorage.setItem('github-last-fetched', String(Date.now()));
         localStorage.setItem('github-events', JSON.stringify(events));
