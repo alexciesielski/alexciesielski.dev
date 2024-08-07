25f18a0e7a1d746ec861419d93a917b347908c14
Alexander Ciesielski <alexander@ciesielski.co>
2024-08-07 16:53:12 +0200

    style: availability card on mobile

--- app/src/app/pages/(landing)/availability.component.ts
+++ app/src/app/pages/(landing)/availability.component.ts
@@ -3,7 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'ac-availability',
   standalone: true,
-  template: ` <p class="text-2xl font-normal tracking-tight lg:text-7xl">Available for work</p> `,
+  template: `
+    <p class="grid h-full place-content-center text-2xl font-normal tracking-tight lg:block lg:h-auto lg:text-7xl">
+      Available for work
+    </p>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AvailabilityComponent {}
